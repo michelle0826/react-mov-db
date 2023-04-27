172998e7cccea55ec16904a329eea72776684543
Michelle Chan <c_michellec@hotmail.com>
2023-04-27 12:20:02 -0700

    nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,27 @@
 import { NavLink } from 'react-router-dom';
 
-const Nav = ({showHideNav}) => {
-    
-    function closeNav(e){
+const Nav = ({ showHideNav }) => {
+
+    function closeNav(e) {
         e.target.blur();
         showHideNav();
     }
     return (
         <nav className="main-nav" onClick={closeNav}>
-        <ul>
-            <li><NavLink to="/About">About</NavLink></li>
-            <li><NavLink to="/Watchlist">Watchlist</NavLink></li>
-        </ul>
-    </nav>
+            <ul>
+                <li><NavLink to="/About">About</NavLink></li>
+                <li><NavLink to="/Watchlist">Watchlist</NavLink></li>
+            </ul>
+            {/* <form>
+                <div className='search-container'>
+                    <input type='search' id='search' />
+                    <button type='submit'>Search</button>
+
+                </div>
+            </form> */}
+
+
+        </nav>
     )
 }
 
