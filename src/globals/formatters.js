8eff155e85a85d8c731ec5196dd26367b00acb5d
juls-ca <juls.devworks@gmail.com>
2023-05-07 19:15:07 -0700

    Edit rounding of movie vote_average

--- src/globals/formatters.js
+++ src/globals/formatters.js
@@ -1,7 +1,7 @@
 export const formatTitle = (title) =>
 title.length > 30 ? title.slice(0, 30) + '...': title;
 
-export const formatRating = (rating) => (rating * 10).toFixed(1) + '%';
+export const formatRating = (rating) => (rating * 10).toFixed(0) + '%';
 
 export function formatOverview(overview, maxLength) {
   // check if 'overview' is falsy (empty/undefined) -> will cause error on splice
