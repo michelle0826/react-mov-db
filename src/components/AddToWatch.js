9545909f17dea58ef8d7fa38ee74a910fcb7aa3a
Kate Khymochka <kkhymochka@my.bcit.ca>
2023-05-07 19:37:02 -0700

    "add to watchlist" functionality done

--- src/components/AddToWatch.js
+++ src/components/AddToWatch.js
@@ -1,23 +1,33 @@
-import {useState} from 'react';
-import {setStorage} from '../utilities/localStorageUtils';
+import { useState } from 'react';
+import { setStorage, getStorage, removeMovieFromStorage } from '../utilities/localStorageUtils';
+import { IoIosAddCircleOutline, IoIosAddCircle } from 'react-icons/io';
 
 function AddToWatch(props) {
-    const [watchlistMovies, setWatchlistMovies] = useState(props.addedMovies);
+  const [watchlistMovies, setWatchlistMovies] = useState(getStorage('watchlistMovies'));
 
-     
-    function addToWatchlist(movie) {
-        const updatedWatchlist = [...watchlistMovies, movie];
-        setWatchlistMovies(updatedWatchlist);
-        setStorage(updatedWatchlist, "watchlistMovies");
-    }
-    
-    return (
-        <button className="add-watchlist-btn" onClick={() => addToWatchlist(props.movie)}>
-            Add to Watchlist
-        </button>
-    );
-   
+  function addToWatchlist(movie) {
+    const updatedWatchlist = [...watchlistMovies, movie];
+    setWatchlistMovies(updatedWatchlist);
+    setStorage(updatedWatchlist, 'watchlistMovies');
+  }
+  
+  // function to remove the movie from the array once the add to watch button clicked again
+  function removeFromWatchlist(index) {
+    const updatedWatchlist = [...watchlistMovies];
+    updatedWatchlist.splice(index, 1);
+    setWatchlistMovies(updatedWatchlist);
+    removeMovieFromStorage(index, 'watchlistMovies');
   }
 
+  const isMovieAdded = watchlistMovies.some((addedMovie) => addedMovie.id === props.movie.id);
+  const addedMovieIndex = watchlistMovies.findIndex((addedMovie) => addedMovie.id === props.movie.id);
+
+
+  return (
+    <button className="add-watchlist-btn" onClick={isMovieAdded ? () => removeFromWatchlist(addedMovieIndex) : () => addToWatchlist(props.movie)}>
+      {isMovieAdded ? <IoIosAddCircle /> : <IoIosAddCircleOutline/>}
+    </button>
+  );
+}
 
 export default AddToWatch;
