2dad26a58a0be88a9d173b33a0d1af4a8cc1db95
Michelle Chan <c_michellec@hotmail.com>
2023-05-09 16:52:14 -0700

    styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from 'react-router-dom';
 import { HiOutlineSearch } from 'react-icons/hi';
-import Header from "./Header";
+
 
 
 function SearchBar(props) {
