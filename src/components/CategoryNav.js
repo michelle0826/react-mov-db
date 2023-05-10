0c55b7a115f08666f379a1c58b8917afc6abc583
juls-ca <juls.devworks@gmail.com>
2023-05-09 22:16:51 -0700

    Add nav transition

--- src/components/CategoryNav.js
+++ src/components/CategoryNav.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 function CategoryNav() {
   const [isOpen, setIsOpen] = useState(false); // shows/hides links
-  const [selectedOption, setSelectedOption] = useState('Select option'); // display selected link on button
+  const [selectedOption, setSelectedOption] = useState('Popular'); // display selected link on button
 
   function handleOptionClick(option) {
     setSelectedOption(option);
@@ -20,21 +20,32 @@ function CategoryNav() {
     <section className='category-section'>
       <p>Browse by: </p>
       <nav className='category-nav' onClick={handleNavClick}>
-        <p>{selectedOption}</p>
-        <ul className={ isOpen || window.innerWidth >= 768 ? 'show' : ''}>
-          <li onClick={() => handleOptionClick('Popular')}>
+        {/* when isOpen is true and on smaller screens, the p tag that displays the current selectedOption is hidden so that only the li's are shown */}
+        <p style={{ display: isOpen || window.innerWidth >= 768 ? 'none' : 'block'}}>{selectedOption}</p>
+
+        {/* visibility is conditional to state and window width */}
+        <ul className={ isOpen || window.innerWidth >= 768 ? 'show' : null}>
+
+          <li className={selectedOption === 'Popular' ? 'selected' : null} onClick={() => handleOptionClick('Popular')}>
             <NavLink to={"/category/popular"}>Popular</NavLink>
           </li>
-          <li onClick={() => handleOptionClick('Now Playing')}>
+
+          <li className={selectedOption === 'Now Playing' ? 'selected' : null} onClick={() => handleOptionClick('Now Playing')}>
             <NavLink to={"/category/now-playing"}>Now Playing</NavLink>
           </li>
-          <li onClick={() => handleOptionClick('Upcoming')}>
+
+          <li className={selectedOption === 'Upcoming' ? 'selected' : null} onClick={() => handleOptionClick('Upcoming')}>
             <NavLink to={"/category/upcoming"}>Upcoming</NavLink>
           </li>
-          <li onClick={() => handleOptionClick('Top Rated')}>
+
+          <li className={selectedOption === 'Top Rated' ? 'selected' : null} onClick={() => handleOptionClick('Top Rated')}>
             <NavLink to={"/category/top-rated"}>Top Rated</NavLink>
           </li>
+
         </ul>
+
+        {selectedOption && <span className={`tab tab-${selectedOption.toLowerCase().replace(/\s/g, '-')}`}></span>}
+          
       </nav>
     </section>
   );
