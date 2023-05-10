2870cd0bb7547a78eb1b154e37bca3aa119f1d65
Kate Khymochka <kkhymochka@my.bcit.ca>
2023-05-09 18:23:08 -0700

    increased the length of the overview on bigger posters, adjusted the info positioning on the grid

--- src/components/AddToWatch.js
+++ src/components/AddToWatch.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { setStorage, getStorage, removeMovieFromStorage } from '../utilities/localStorageUtils';
-import { IoIosAddCircleOutline, IoIosAddCircle } from 'react-icons/io';
+import { BsPlusCircle, BsPlusCircleFill } from "react-icons/bs";
+
 
 function AddToWatch(props) {
   const [watchlistMovies, setWatchlistMovies] = useState(getStorage('watchlistMovies'));
@@ -11,21 +12,20 @@ function AddToWatch(props) {
     setStorage(updatedWatchlist, 'watchlistMovies');
   }
   
-  // function to remove the movie from the array once the add to watch button clicked again
+  // to remove the movie from the array once the add to watch button clicked again
   function removeFromWatchlist(index) {
     const updatedWatchlist = [...watchlistMovies];
     updatedWatchlist.splice(index, 1);
     setWatchlistMovies(updatedWatchlist);
     removeMovieFromStorage(index, 'watchlistMovies');
   }
-
   const isMovieAdded = watchlistMovies.some((addedMovie) => addedMovie.id === props.movie.id);
   const addedMovieIndex = watchlistMovies.findIndex((addedMovie) => addedMovie.id === props.movie.id);
 
 
   return (
     <button className="add-watchlist-btn" onClick={isMovieAdded ? () => removeFromWatchlist(addedMovieIndex) : () => addToWatchlist(props.movie)}>
-      {isMovieAdded ? <IoIosAddCircle /> : <IoIosAddCircleOutline/>}
+      {isMovieAdded ? <BsPlusCircleFill /> : <BsPlusCircle/>}
     </button>
   );
 }
