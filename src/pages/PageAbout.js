d8e71046ad0c9dd021b948770bde11fa4f53dc2e
juls-ca <juls.devworks@gmail.com>
2023-05-14 08:39:35 -0700

    Edit About content position

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -4,9 +4,9 @@ import tmdbLogo from "../images/tmdb-logo.svg";
 
 function PageAbout() {
     return(
-        <main>
+        <main className="about-page">
             <PageTitle title="About Slate — Slate Movie Database" />
-            <section className="about-page">
+            <section className="about-section">
                 <h1>Your Ultimate Cinematic Guide</h1>
                 <p>
                     Browse Popular, Top Rated, Now Playing, and Upcoming movies on SLATE. Search for titles, check out their ratings, and see how they match up!
